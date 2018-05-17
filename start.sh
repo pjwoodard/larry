40db42c37af17307ba4032a48d033661df51e07e
Bijan Sondossi <bijn@users.noreply.github.com>
2018-05-17 13:58:58 -0700

    exit if pkcs11_library_path is undefined

--- start.sh
+++ start.sh
@@ -37,6 +37,7 @@ fi
 if [ -z "$PKCS11_LIBRARY_PATH" ]
 then 
     echo "Warning, PKCS11_LIBRARY_PATH is undefined." >&2
+    exit
 fi
 
 python3 $web2py_path/web2py.py -a 'password' -i 127.0.0.1 -p 8080 -e
