b4640ea7d5f9e2834f9c552db09657b2c1a36b12
Bijan Sondossi <bijan.n.sondossi@gmail.com>
2018-06-09 17:42:41 -0700

    issue #36 merged with master

--- source/static/js/sign_verify.js
+++ source/static/js/sign_verify.js
@@ -2,12 +2,15 @@ function Signer()
 {
     // Variables -------------------------------------------------------
 
-    this.data = "";
-    this.signed_data = "";
+    this.data = null;
+    this.signed_data = null;
     this.enabled = false;
 
     this.key = null;
     this.sign_mech = null;
+
+    this.key_error = false;
+    this.data_error = false;
 
     // Member functions ------------------------------------------------
 
@@ -16,6 +19,28 @@ function Signer()
         this.sign_mech = null;
         this.data = "";
         this.signed_data = "";
+    }
+
+    this.clear_errors = function() {
+        this.key_error = false;
+        this.data_error = false;
+    };
+
+    this.validate_form = function() {
+        validated = true;
+
+        this.clear_errors();
+
+        if(this.key == null) {
+            this.key_error = true;
+            validated = false;
+        }
+        if(this.data == null) {
+            this.data_error = true;
+            validated = false;
+        }
+
+        return validated;
     };
 
     this.key_type = function() {
@@ -58,38 +83,42 @@ function Signer()
     };
 
     this.sign = function() {
-        $.post(
-            sign_url,
-            {
-                obj_type: this.key_type(),
-                label: this.key.p11_label,
-                object_id: this.key.p11_id,
-                mech: this.sign_mech,
-                data: this.data,
-            }, function (data) {
-                console.log(data.signed_data);
-            }
-        );
+        if(this.validate_form()) {
+            $.post(
+                sign_url,
+                {
+                    obj_type: this.key_type(),
+                    label: this.key.p11_label,
+                    object_id: this.key.p11_id,
+                    mech: this.sign_mech,
+                    data: this.data,
+                }, function (data) {
+                    console.log(data.signed_data);
+                }
+            );
 
-        this.reset();
+            this.reset();
+        }
     };
 
     this.verify = function() {
-        console.log(this.signed_data);
-        $.post(
-            verify_url,
-            {
-                obj_type: this.key_type(),
-                label: this.key.p11_label,
-                object_id: this.key.p11_id,
-                mech: this.sign_mech,
-                data: this.data,
-                signed_data: this.signed_data,
-            }, function (data) {
+        if(this.validate_form()) {
+            console.log(this.signed_data);
+            $.post(
+                verify_url,
+                {
+                    obj_type: this.key_type(),
+                    label: this.key.p11_label,
+                    object_id: this.key.p11_id,
+                    mech: this.sign_mech,
+                    data: this.data,
+                    signed_data: this.signed_data,
+                }, function (data) {
+                    console.log(data.is_valid_signature);
+                }
+            );
 
-            }
-        );
-
-        this.reset();
+            this.reset();
+        }
     };
 }
